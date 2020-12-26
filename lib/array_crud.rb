a37144bf584a5709c060a77f5cd89d96601cdda8
Dayne Dewar <daynedewar@yahoo.com>
2020-12-26 23:02:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,24 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  dog_names = ["Sora", "Hova", "Rudy", "Kira"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  dog_names = ["Sora", "Hova", "Rudy", "Kira"]
+  dog_names << ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  dog_names = ["Sora", "Hova", "Rudy", "Kira"]
+  dog_names.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  dog_names = ["Sora", "Hova", "Rudy", "Kira", "arrays!"]
+  dog_names.pop
 end
 
 def remove_element_from_start_of_array(array)
